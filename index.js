60ca0539b36a67138f1c8d157df6f0f931555d65
wmoralesdev <walterrafael26@gmail.com>
2021-02-02 20:32:29 -0600

    Modify for template ussage

--- index.js
+++ index.js
@@ -1,98 +1,15 @@
-const { app, BrowserWindow, ipcMain, Tray, Menu } = require('electron')
-const fs = require('fs')
-
-let win = [],
-    jsonSave = [],
-    tray, trayMenu, defaultX = 0,
-    defaultY = 0
-
-trayMenu = Menu.buildFromTemplate([
-    { label: 'Salir', click: app.quit },
-    { label: 'Guardar y salir', click: onAppExitSaveHandler }
-])
-
-function createTray() {
-    tray = new Tray('src/img/trayTemplate.png')
-    tray.setContextMenu(trayMenu)
-}
+const { app, BrowserWindow } = require('electron')
 
 function createWindow() {
-    var { screen } = require('electron')
-
-    if (win.length == 0) {
-        defaultX = screen.getPrimaryDisplay().bounds.width - 300
-        console.log(defaultX);
-    }
-
-    if (defaultX < 0) {
-        defaultX = screen.getPrimaryDisplay().bounds.width - 300
-        defaultY += 500
-    }
-
-    let lastWindow = new BrowserWindow({
-        width: 300,
-        height: 500,
-        x: defaultX,
-        y: defaultY,
-        frame: false,
+    const win = new BrowserWindow({
+        width: 800,
+        height: 800,
         webPreferences: {
             nodeIntegration: true
         }
     })
 
-    defaultX -= 300
-
-    lastWindow.once('close', () => {
-        win.splice(win.indexOf(lastWindow), 1)
-    })
-
-    lastWindow.loadFile('src/index.html')
-
-    win.push(lastWindow)
+    win.loadFile('src/index.html')
 }
 
-async function fileSave() {
-    var path = app.getPath('userData')
-
-    if (process.platform == 'win32')
-        path += '\\config.json'
-    else
-        path += '/config.json'
-
-    console.log(path);
-
-    fs.writeFile(path, JSON.stringify(jsonSave), err => {
-        if (err)
-            new Notification('Error', {
-                body: 'Algo salio mal',
-                icon: 'src/img/trayTemplate.png'
-            })
-        else {
-            app.quit()
-        }
-    })
-}
-
-function onAppExitSaveHandler() {
-    win.forEach(w => {
-        w.webContents.send('saveRequested')
-    })
-}
-
-ipcMain.on('saveResponse', (e, args) => {
-    if (!jsonSave.includes(args))
-        jsonSave.push(args)
-
-    if (jsonSave.length == BrowserWindow.getAllWindows().length)
-        fileSave()
-})
-
-ipcMain.on('new-note', (e, args) => {
-    createWindow()
-    e.sender.send('created-window', 'Window created')
-})
-
-app.on('ready', () => {
-    createTray()
-    createWindow()
-})
+app.on('ready', createWindow)
